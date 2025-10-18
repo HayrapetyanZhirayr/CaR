65d4dacfa64b3071acdeacecf1f8db305fb7f4f7
Hayrapetyan Zhirayr <zhsayrapetyan@yandex.ru>
2025-10-18 12:49:32 +0000

    add run script

---
+++ run.sh
@@ -0,0 +1,3 @@
+python Ranking/split_IQS.py \
+    --input="/workspace/hayrapetyan/GG_data/Qwen2.5-72B-IT-CFA/data/graphgen/1760391060/evaluation.json" \
+    --output="/workspace/hayrapetyan/GG_data/Qwen2.5-72B-IT-CFA/data/graphgen/1760391060/evaluation_IQS.json" \
